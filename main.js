4c4326ebe64cc95e95ecab55e0aa358e2d3db6de
Fajarshodiq1 <fajarshodiq1220@gmail.com>
2024-02-16 23:59:13 +0700

    update6

---
+++ main.js
@@ -0,0 +1,53 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+ScrollReveal().reveal(".h4_hero");
+ScrollReveal().reveal(".info_galery", { delay: 100 });
+ScrollReveal().reveal(".h1_hero", { delay: 100 });
+ScrollReveal().reveal(".mid_photo1", { delay: 500 });
+ScrollReveal().reveal(".mid_photo2", { delay: 500 });
+ScrollReveal().reveal(".span_hero", { delay: 1000 });
+ScrollReveal().reveal(".p_hero", { delay: 1200 });
+ScrollReveal().reveal(".simply_hero", { delay: 1400 });
+ScrollReveal().reveal(".btn_invit", { delay: 1600 });
+ScrollReveal().reveal(".text_home", { delay: 100 });
+ScrollReveal().reveal(".iframe_information", { delay: 100 });
+ScrollReveal().reveal(".story_title", { delay: 100 });
+ScrollReveal().reveal(".timeline_image", { delay: 100 });
+ScrollReveal().reveal(".f-title", { delay: 50 });
+ScrollReveal().reveal(".bi-instagram", { delay: 100 });
+ScrollReveal().reveal(".bi-github", { delay: 300 });
+ScrollReveal().reveal(".bi-facebook", { delay: 500 });
+ScrollReveal().reveal(".bi-twitter", { delay: 700 });
+ScrollReveal().reveal(".footer_bg", { delay: 900 });
+ScrollReveal().reveal(".timeline_image2", { delay: 500 });
+ScrollReveal().reveal(".timeline_image3", { delay: 1000 });
+ScrollReveal().reveal(".timeline_image4", { delay: 1500 });
+
+const srLeft = ScrollReveal({
+  origin: "left",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+srLeft.reveal(".row1_home", { delay: 500 });
+srLeft.reveal(".left_photo1", { delay: 500 });
+srLeft.reveal(".left_photo2", { delay: 500 });
+srLeft.reveal(".akad_information", { delay: 500 });
+srLeft.reveal(".timeline_panel", { delay: 500 });
+srLeft.reveal(".timeline_panel3", { delay: 1500 });
+const srRight = ScrollReveal({
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+srLeft.reveal(".row2_home", { delay: 500 });
+srLeft.reveal(".right_photo1", { delay: 500 });
+srLeft.reveal(".resepsi_information", { delay: 500 });
+srLeft.reveal(".timeline_panel2", { delay: 1000 });
+srLeft.reveal(".timeline_panel4", { delay: 2000 });
